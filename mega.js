c1879264d51331cd6c9aa15272b5332aa3115aa5
saasipro <saasifypro@gmail.com>
2025-06-08 07:34:35 +0500

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,7 +1,7 @@
 const mega = require("megajs");
 const auth = {
-    email: 'rokima5506@claspira.com',
-    password: 'hp73!WyZ-XWDsM:',
+    email: 'ledefa8974@jio1.com',
+    password: 'muskanqueen&&584',
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 }
 
